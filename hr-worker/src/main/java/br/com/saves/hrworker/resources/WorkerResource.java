b46f83809695693681b41372934545044d35b046
Christian Costa <christian_costa@terceiros.sicredi.com.br>
2021-01-30 13:22:29 -0400

    Zuul timeout

--- hr-worker/src/main/java/br/com/saves/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/saves/hrworker/resources/WorkerResource.java
@@ -31,6 +31,13 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         log.info("PORT = "+ env.getProperty("local.server.port"));
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
