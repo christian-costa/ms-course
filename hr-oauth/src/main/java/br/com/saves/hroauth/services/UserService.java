6402d81dc9d354645465f7566bb56c21af2a5dff
Christian Costa <christian_costa@terceiros.sicredi.com.br>
2021-01-31 11:54:21 -0400

    Login jwt token

--- hr-oauth/src/main/java/br/com/saves/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/saves/hroauth/services/UserService.java
@@ -4,12 +4,15 @@ import br.com.saves.hroauth.entities.User;
 import br.com.saves.hroauth.feingclients.UserFeingClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private UserFeingClient userFeingClient;
 
@@ -18,6 +21,17 @@ public class UserService {
         if(user == null) {
             log.error("Email not found: "+email);
             throw new IllegalArgumentException("Email not found");
+        }
+        log.info("Email found: "+email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userFeingClient.findByEmail(email).getBody();
+        if(user == null) {
+            log.error("Email not found: "+email);
+            throw new UsernameNotFoundException("Email not found");
         }
         log.info("Email found: "+email);
         return user;
